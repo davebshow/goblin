5897ea9b4545fb09e966be41e4a59d7588e11317
davebshow <davebshow@gmail.com>
2016-07-06 12:17:20 -0400

    simplify driver

--- goblin/driver/api.py
+++ goblin/driver/api.py
@@ -16,25 +16,9 @@ class GremlinServer:
                    force_close: bool=False,
                    username: str=None,
                    password: str=None) -> connection.Connection:
-        # Use connection factory here
         if client_session is None:
             client_session = aiohttp.ClientSession(loop=loop)
         ws = await client_session.ws_connect(url)
         return connection.Connection(ws, loop, client_session,
                                      force_close=force_close,
                                      username=username, password=password)
-
-    @classmethod
-    async def create_client(cls,
-                            url: str,
-                            loop: asyncio.BaseEventLoop,
-                            *,
-                            client_session: aiohttp.ClientSession=None,
-                            max_inflight: int=None,
-                            max_connections: int=None,
-                            force_close: bool=False,
-                            force_release: bool=False,
-                            pool: pool.Pool=None,
-                            username: str=None,
-                            password: str=None) -> connection.Connection:
-        pass
