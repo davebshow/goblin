048fea1c396c4cdad71dfc87a2fead78f7157e42
davebshow <davebshow@gmail.com>
2016-07-15 17:33:24 -0400

    added Generic Elements to suppress list

--- goblin/element.py
+++ goblin/element.py
@@ -29,7 +29,8 @@ class ElementMeta(type):
             new_namespace[k] = v
         new_namespace['__mapping__'] = mapper.create_mapping(namespace,
                                                              props)
-        if name not in ('Element', 'Vertex', 'Edge', 'VertexProperty'):
+        if name not in ('Element', 'Vertex', 'Edge', 'VertexProperty'
+                        'GenericVertex', 'GenericEdge'):
             logger.warning("Creating new Element class {}: {}".format(
                 name, new_namespace['__mapping__']))
         result = type.__new__(cls, name, bases, new_namespace)
